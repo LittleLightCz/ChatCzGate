958177de0aba78cd9201227a739ca03fb672137e
LittleLightCz <antonin.brettsnajdr@gmail.com>
2015-12-27 21:31:13 +0100

    Added get_room_list()

--- src/chatapi.py
+++ src/chatapi.py
@@ -1,4 +1,44 @@
+import logging as log
+import requests as req
 from bs4 import BeautifulSoup
+
+
+CHAT_CZ_URL = "https://chat.cz"
+
+#-------------------------------------------------------------------------------
+#Temporary logger init ... will be moved to main script file afterwards ...
+
+LOGGER_FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+
+#Init logger
+log.basicConfig(level=log.DEBUG,
+                format=LOGGER_FORMAT,
+                filename='log.txt',
+                filemode='a')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = log.StreamHandler()
+console.setLevel(log.DEBUG)
+# set a format which is simpler for console use
+formatter = log.Formatter(LOGGER_FORMAT)
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+log.getLogger('').addHandler(console)
+
+#-------------------------------------------------------------------------------
+
+
+class Room:
+    """
+    Data holder class for room information
+    """
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return "{0}\t ({1})".format(self.name, self.description)
 
 
 class ChatAPI:
@@ -8,4 +48,23 @@ class ChatAPI:
 
     def __init__(self):
         pass
+
+    def get_room_list(self):
+        """
+        Returns the list of all rooms on the server sorted by name. Data are held in the class Room.
+        """
+        resp = req.get(CHAT_CZ_URL)
+        html = BeautifulSoup(resp.text, "html.parser")
+
+        def _to_room(div):
+            name = div.a.h4.text.strip()
+            description = div.a.text.replace(name,"",1).replace("[\n\r]","").strip()
+            return Room(name, description)
+
+        divs = html.find_all("div","row row-xs-height list-group")
+        rooms = [ _to_room(div) for div in divs ]
+        rooms.sort(key = lambda r: r.name)
+        return rooms
+
+
 
