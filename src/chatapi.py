9b9fb6dba9500e2683480a8c4f61f39a1289cc39
LittleLightCz <antonin.brettsnajdr@gmail.com>
2015-12-28 12:12:25 +0100

    Added login_as_anonymous, but it doesn't work yet :-)

--- src/chatapi.py
+++ src/chatapi.py
@@ -6,6 +6,10 @@ from bs4 import BeautifulSoup
 
 
 CHAT_CZ_URL = "https://chat.cz"
+LOGIN_URL = CHAT_CZ_URL + "/login"
+LOGOUT_URL = CHAT_CZ_URL + "/logout"
+
+JSON_HEADER_URL = CHAT_CZ_URL + "/json/getHeader"
 
 #-------------------------------------------------------------------------------
 #Temporary logger init ... will be moved to main script file afterwards ...
@@ -31,8 +35,8 @@ log.getLogger('').addHandler(console)
 
 class Gender(Enum):
     """Enum for gender"""
-    MALE = 1
-    FEMALE = 2
+    MALE = "m"
+    FEMALE = "f"
 
 
 class Room:
@@ -54,12 +58,20 @@ class ChatAPI:
     """
 
     def __init__(self):
+        log.info("Getting cookie ...")
+        self.cookies = req.get(LOGIN_URL).cookies
+
+        self.headers = {
+            "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
+        }
+        log.debug(self.headers)
         pass
 
     def get_room_list(self):
         """
         Returns the list of all rooms on the server sorted by name. Data are held in the class Room.
         """
+        log.info("Downloading room list ...")
         resp = req.get(CHAT_CZ_URL)
         html = BeautifulSoup(resp.text, "html.parser")
 
@@ -71,7 +83,27 @@ class ChatAPI:
         divs = html.find_all("div","row row-xs-height list-group")
         rooms = [ to_room(div) for div in divs ]
         rooms.sort(key = lambda r: r.name)
+
+        log.debug([r.name for r in rooms])
         return rooms
 
+
+    def login_as_anonymous(self,user,gender = Gender.MALE):
+        """
+        Logins to the server as an anonymous user.
+
+        Parameters:
+            user : string
+                   Username
+            gender : Gender
+                     Gender from Gender enum. Default is MALE.
+        """
+        params = {"nick" : user, "sex" : gender.value}
+
+        log.info("Logging anonymously as: {0} ({1})".format(user,gender.value))
+        resp = req.post(LOGIN_URL, headers=self.headers, params=params, cookies=self.cookies)
+        self.cookies.update(resp.cookies)
+
+        print("nav-user" in resp.text)
 
 
