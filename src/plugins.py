5f0382710bf860ddd17e2f8d8ec08d4257f4cfe1
LittleLightCz <antonin.brettsnajdr@gmail.com>
2016-01-03 12:42:31 +0100

    Added stubs for plugins

--- src/plugins.py
+++ src/plugins.py
@@ -2,6 +2,25 @@ import os
 import re
 import sys
 
+class PluginData:
+    """
+    Data holder class that is passed to each plugin individually to process its content
+    """
+    def __init__(self, reply=None, command=None, result_replies=[], result_commands=[]):
+        self.reply = reply
+        self.command = command
+        self.result_replies = result_replies
+        self.result_commands = result_commands
+
+    def verify_results(self):
+        """
+        This method makes sure that results will be appropriately set if no plugin takes any action at all.
+        """
+        if self.reply:
+            self.result_replies = self.result_replies or [self.reply]
+
+        if self.command:
+            self.result_commands = self.result_commands or [self.command]
 
 def import_plugins(path='plugins'):
     sys.path.append(path)
@@ -11,6 +30,23 @@ def import_plugins(path='plugins'):
             match = re.search("(.+)\.py(c?)$", entry)
             if match:
                 globals()[match.groups()[0]] = __import__(match.groups()[0])
+
+
+def process(data):
+    """
+    Process data
+    :param data: PluginData
+        Data containing reply or command to be processed by the plugins
+    """
+    try:
+        for plugin in plugins:
+            if not plugin.process(data):
+                break
+    except Exception:
+        log.exception("Error occurred in plugin processing")
+
+    # Verify results (in case nothing was processed)
+    data.verify_results()
 
 
 # example
--- src/plugins/smileys.py
+++ src/plugins/smileys.py
@@ -1,5 +1,13 @@
 
+name = "Smileys"
 
-class SmileysPlugin:
-    def __init__(self):
-        print("init smileys debug")
+def process(data):
+    """
+    Process data
+    :param data: PluginData
+    :return True if data should be processed by another plugin in the list, or False otherwise
+    """
+
+    # Process data ....
+
+    return True
